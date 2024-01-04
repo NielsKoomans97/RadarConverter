503e076585a8e02ce43bae76806d6af958ce9de7
Niels <44090733+NielsKoomans97@users.noreply.github.com>
2024-01-04 14:56:05 +0100

    Update Program.cs
    ja dit is wel netjes opzich

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Drawing;
 using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
 using static System.Net.Mime.MediaTypeNames;
 using Image = System.Drawing.Image;
 
@@ -9,7 +10,15 @@ namespace RadarConverter
     {
         private static async Task Main(string[] args)
         {
-            if (args[0] == "update")
+            if (args[0] == "comparecolors")
+            {
+                var color = Color.FromName(args[1]);
+                var indexInt = Convert.ToInt32(args[2]);
+
+                Console.WriteLine($"Unchanged\n R:{color.R} G:{color.G} B:{color.B}");
+                Console.WriteLine($"Changed\n R:{Converter.CalculateColorIntensity(color.R, indexInt)} G:{Converter.CalculateColorIntensity(color.G, indexInt)} B:{Converter.CalculateColorIntensity(color.B, indexInt)}");
+            }
+            else if (args[0] == "update")
             {
                 await UpdateChartsAsync();
             }
@@ -103,9 +112,8 @@ namespace RadarConverter
         public string[] PrecipTypeMaps { get; set; }
         public string OutputPath { get; set; }
 
-        private Color[] IntensityColors => new Color[]
+        private static Color[] IntensityColors => new Color[]
         {
-                Color.White,
                 Color.FromArgb(211,211,211),
                 Color.FromArgb(181,191,185),
                 Color.FromArgb(152,171,159),
@@ -145,29 +153,37 @@ namespace RadarConverter
                 Color.FromArgb(161,161,161),
                 Color.FromArgb(178,178,178),
                 Color.FromArgb(194,194,194),
-        }.Reverse().ToArray();
+        };
 
         private Color[] RainIntensityColors = new Color[]
         {
             Color.FromArgb(0,87,138),
             Color.FromArgb(0,131,203),
+            Color.FromArgb(0,110,170),
             Color.FromArgb(94,167,220),
             Color.FromArgb(148,191,231),
+            Color.FromArgb(190, 221, 250),
+            Color.FromArgb(227, 241, 255),
+            Color.FromArgb(17, 148, 211),
         }.Reverse().ToArray();
 
         private Color[] SnowIntensityColors = new Color[]
         {
-            Color.FromArgb(206,87,161),
-            Color.FromArgb(215,129,182),
-            Color.FromArgb(227,168,204),
+            Color.FromArgb(206, 87, 161),
+            Color.FromArgb(215, 129, 182),
+            Color.FromArgb(227, 168, 204),
+            Color.FromArgb(224, 188, 210),
+            Color.FromArgb(240, 223, 233),
         }.Reverse().ToArray();
 
         private Color[] SleetIntensityColors = new Color[]
         {
-            Color.FromArgb(255,232,0),
-            Color.FromArgb(255,235,79),
-            Color.FromArgb(255,240,131),
-            Color.FromArgb(255,245,173),
+            Color.FromArgb(255, 232, 0),
+            Color.FromArgb(255, 235, 79),
+            Color.FromArgb(255, 240, 131),
+            Color.FromArgb(255, 245, 173),
+            Color.FromArgb(252, 246, 202),
+            Color.FromArgb(255, 253, 235),
         }.Reverse().ToArray();
 
         private bool HasIntensityColor(Color pixel)
@@ -193,14 +209,27 @@ namespace RadarConverter
             }
         }
 
-        private int CalculateColorIntensity(int colorValue, int index)
+        public static int CalculateColorIntensity(int colorValue, int index)
         {
-            int minvalue = ((colorValue / 25) * index);
-            int newvalue = 100 - minvalue;
+            //int minvalue = ((colorValue / IntensityColors.Length) * index);
+            //int newvalue = 75 - minvalue;
 
-            colorValue = colorValue - newvalue;
+            //colorValue = colorValue - newvalue;
 
-            return colorValue;
+            if (colorValue > 0)
+            {
+                var newColorValue = 255 - colorValue;
+                double newIndex = Math.Round((double)index / IntensityColors.Length, 2);
+
+                //Console.WriteLine($"newColorValue = {newColorValue}\nnewIndex = {newIndex}");
+
+                var calc = newColorValue * newIndex;
+
+                //Console.WriteLine(calc.ToString());
+                return Convert.ToInt32(255 - calc);
+            }
+
+            return 0;
         }
 
         private Color SetColorIntensity(Color color, int index)
@@ -209,9 +238,9 @@ namespace RadarConverter
             int g = CalculateColorIntensity(color.G, index);
             int b = CalculateColorIntensity(color.B, index);
 
-            if (r < 0) r = 0;
-            if (g < 0) g = 0;
-            if (b < 0) b = 0;
+            if (r < 0) r = color.R;
+            if (g < 0) g = color.G;
+            if (b < 0) b = color.B;
 
             if (r > 255) r = 255;
             if (g > 255) g = 255;
@@ -236,26 +265,55 @@ namespace RadarConverter
 
                         var index = GetIndex(radarPixel);
 
-                        if (typePixel != Color.FromArgb(255, 255, 255))
+                        if (typePixel != Color.White)
                         {
                             if (RainIntensityColors.Contains(typePixel))
                             {
-                                lastTypeColor = RainIntensityColors[2];
+                                lastTypeColor = RainIntensityColors.Last();
                             }
 
                             if (SleetIntensityColors.Contains(typePixel))
                             {
-                                lastTypeColor = SleetIntensityColors[2];
+                                lastTypeColor = SleetIntensityColors.Last();
                             }
 
                             if (SnowIntensityColors.Contains(typePixel))
                             {
-                                lastTypeColor = SnowIntensityColors[2];
+                                lastTypeColor = SnowIntensityColors.Last();
                             }
                         }
                         else
                         {
-                            lastTypeColor = radarPixel;
+                            int y2 = y;
+
+                            do
+                            {
+                                typePixel = type.GetPixel(x, y2);
+
+                                if (typePixel != Color.White)
+                                {
+                                    if (RainIntensityColors.Contains(typePixel))
+                                    {
+                                        lastTypeColor = RainIntensityColors.Last();
+                                    }
+
+                                    if (SleetIntensityColors.Contains(typePixel))
+                                    {
+                                        lastTypeColor = SleetIntensityColors.Last();
+                                    }
+
+                                    if (SnowIntensityColors.Contains(typePixel))
+                                    {
+                                        lastTypeColor = SnowIntensityColors.Last();
+                                    }
+
+                                    return;
+                                }
+                                else
+                                {
+                                    y2++;
+                                }
+                            } while (y2 < (radar.Height - y));
                         }
 
                         radar.SetPixel(x, y, SetColorIntensity(lastTypeColor, index));
